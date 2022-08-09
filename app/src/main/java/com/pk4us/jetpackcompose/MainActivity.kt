ba884d9d0a0d7be0de6ed9f968ccbae89371eb1f
PK4U_S <Stepanchuk.ei@gmail.com>
2022-08-09 18:45:08 +0300

    Column и Row

--- app/src/main/java/com/pk4us/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/pk4us/jetpackcompose/MainActivity.kt
@@ -3,41 +3,48 @@ package com.pk4us.jetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import com.pk4us.jetpackcompose.ui.theme.JetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+            Row(
+                Modifier
+                    .background(Color.Gray).fillMaxHeight(0.5f)
+                    .fillMaxSize(),
+                horizontalArrangement = Arrangement.SpaceEvenly){
+
+                Column(modifier = Modifier
+                    .background(Color.Red).fillMaxWidth(0.3f).fillMaxHeight(0.8f),
+                    verticalArrangement = Arrangement.SpaceAround,
+                    horizontalAlignment = Alignment.CenterHorizontally,
+
                 ) {
-                    Greeting("Android")
+                    Text(text = "Hello")
+                    Text(text = "dear")
+                    Text(text = "friend")
                 }
+
+                Column(modifier = Modifier
+                    .background(Color.Green).fillMaxHeight(0.5f),
+                    verticalArrangement = Arrangement.SpaceAround,
+                    horizontalAlignment = Alignment.End,
+
+                    ) {
+                    Text(text = "How")
+                    Text(text = "are")
+                    Text(text = "you")
+                }
+
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    JetpackComposeTheme {
-        Greeting("Android")
     }
 }
