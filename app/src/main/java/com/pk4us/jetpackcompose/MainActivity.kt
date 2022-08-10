e00160409bb1dbd338441e549506f17c289269a2
PK4U_S <Stepanchuk.ei@gmail.com>
2022-08-10 13:56:44 +0300

    Modifier

--- app/src/main/java/com/pk4us/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/pk4us/jetpackcompose/MainActivity.kt
@@ -1,10 +1,11 @@
 package com.pk4us.jetpackcompose
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
+import androidx.compose.foundation.*
+import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -15,6 +16,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -23,14 +25,25 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column() {
+            Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
                 listItem(name = "Степанчук Евгений", prof = "Программист")
                 listItem(name = "Степанчук Евгений", prof = "Программист")
                 listItem(name = "Степанчук Евгений", prof = "Программист")
                 listItem(name = "Степанчук Евгений", prof = "Программист")
                 listItem(name = "Степанчук Евгений", prof = "Программист")
-
-
+                listItem(name = "Степанчук Евгений", prof = "Программист")
+                listItem(name = "Степанчук Евгений", prof = "Программист")
+                listItem(name = "Степанчук Евгений", prof = "Программист")
+                listItem(name = "Степанчук Евгений", prof = "Программист")
+                listItem(name = "Степанчук Евгений", prof = "Программист")
+                listItem(name = "Степанчук Евгений", prof = "Программист")
+                listItem(name = "Степанчук Евгений", prof = "Программист")
+                listItem(name = "Степанчук Евгений", prof = "Программист")
+                listItem(name = "Степанчук Евгений", prof = "Программист")
+                listItem(name = "Степанчук Евгений", prof = "Программист")
+                listItem(name = "Степанчук Евгений", prof = "Программист")
+                listItem(name = "Степанчук Евгений", prof = "Программист")
+                listItem(name = "Степанчук Евгений", prof = "Программист")
             }
         }
     }
@@ -38,8 +51,18 @@ class MainActivity : ComponentActivity() {
     @Composable
     fun listItem(name:String,prof:String){
         Card(modifier = Modifier
+            .background(Color.Gray)
             .fillMaxWidth()
-            .padding(10.dp),
+            .padding(10.dp)
+            .pointerInput(Unit){
+                detectDragGesturesAfterLongPress { change, dragAmount ->
+                    Log.d("MyLog", "LongPress")
+
+                }
+            }
+            .clickable {
+                Log.d("MyLog", "Clicked")
+            },
         shape = RoundedCornerShape(15.dp),
         elevation = 5.dp
         ){
