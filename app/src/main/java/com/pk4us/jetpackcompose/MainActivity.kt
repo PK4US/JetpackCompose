392358509328e67c42d9c244bcc7cf61fa099c7f
PK4U_S <Stepanchuk.ei@gmail.com>
2022-08-10 21:40:45 +0300

    Text Expand

--- app/src/main/java/com/pk4us/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/pk4us/jetpackcompose/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.ui.Modifier
@@ -14,13 +15,13 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            LazyRow(modifier = Modifier.background(Gray100).fillMaxWidth()){ itemsIndexed(
-                listOf(ItemRowModel(R.drawable.image1,"Миша"),
-                    ItemRowModel(R.drawable.image2,"Егор"),
-                    ItemRowModel(R.drawable.image3,"Андрей"),
-                    ItemRowModel(R.drawable.image4,"Витя"),
-                    ItemRowModel(R.drawable.image5,"Серьгей"),
-                    ItemRowModel(R.drawable.image6,"Олег"),)){ _,item -> MyRow(item = item) }
+            LazyColumn(modifier = Modifier.background(Gray100).fillMaxWidth()){ itemsIndexed(
+                listOf(ItemRowModel(R.drawable.image1,"Миша","testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest"),
+                    ItemRowModel(R.drawable.image2,"Егор","testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest"),
+                    ItemRowModel(R.drawable.image3,"Андрей","testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest"),
+                    ItemRowModel(R.drawable.image4,"Витя","testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest"),
+                    ItemRowModel(R.drawable.image5,"Серьгей","testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest"),
+                    ItemRowModel(R.drawable.image6,"Олег","testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest"),)){ _,item -> MyRow(item = item) }
             }
         }
     }
