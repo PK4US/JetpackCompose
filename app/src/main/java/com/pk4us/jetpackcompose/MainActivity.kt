73d644b905067b669c3a1b988f5ab789db772404
PK4U_S <Stepanchuk.ei@gmail.com>
2022-08-10 16:41:31 +0300

    LazyColumn

--- app/src/main/java/com/pk4us/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/pk4us/jetpackcompose/MainActivity.kt
@@ -1,51 +1,32 @@
 package com.pk4us.jetpackcompose
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.*
-import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-                CircleItem()
+            LazyColumn(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .fillMaxSize()
+            ) {
+                itemsIndexed(listOf("item1","item2","item3","item4","item5","item6","item7")
+                ){ index,item ->
+                    Text(text = item, fontSize = 30.sp, modifier = Modifier.padding(vertical = 10.dp))
+                }
             }
-        }
-    }
-
-    @Composable
-    fun CircleItem(){
-        val counter = remember{
-            mutableStateOf(0)
-        }
-        val color = remember{
-            mutableStateOf(Color.Blue)
-        }
-        Box(modifier = Modifier.size(100.dp).background(color = color.value, shape = CircleShape).clickable { when(++counter.value) {10->color.value = Color.Green }},contentAlignment = Alignment.Center){
-            Text(text = counter.value.toString(), style = TextStyle(color = Color.White), fontSize = 20.sp)
         }
     }
 }
