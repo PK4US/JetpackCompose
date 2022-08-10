1726327892399034983d2795372a6cea2bad3468
PK4U_S <Stepanchuk.ei@gmail.com>
2022-08-10 15:49:29 +0300

    States

--- app/src/main/java/com/pk4us/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/pk4us/jetpackcompose/MainActivity.kt
@@ -12,6 +12,8 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -19,73 +21,31 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
-                listItem(name = "Степанчук Евгений", prof = "Программист")
-                listItem(name = "Степанчук Евгений", prof = "Программист")
-                listItem(name = "Степанчук Евгений", prof = "Программист")
-                listItem(name = "Степанчук Евгений", prof = "Программист")
-                listItem(name = "Степанчук Евгений", prof = "Программист")
-                listItem(name = "Степанчук Евгений", prof = "Программист")
-                listItem(name = "Степанчук Евгений", prof = "Программист")
-                listItem(name = "Степанчук Евгений", prof = "Программист")
-                listItem(name = "Степанчук Евгений", prof = "Программист")
-                listItem(name = "Степанчук Евгений", prof = "Программист")
-                listItem(name = "Степанчук Евгений", prof = "Программист")
-                listItem(name = "Степанчук Евгений", prof = "Программист")
-                listItem(name = "Степанчук Евгений", prof = "Программист")
-                listItem(name = "Степанчук Евгений", prof = "Программист")
-                listItem(name = "Степанчук Евгений", prof = "Программист")
-                listItem(name = "Степанчук Евгений", prof = "Программист")
-                listItem(name = "Степанчук Евгений", prof = "Программист")
-                listItem(name = "Степанчук Евгений", prof = "Программист")
+            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                CircleItem()
             }
         }
     }
 
     @Composable
-    fun listItem(name:String,prof:String){
-        Card(modifier = Modifier
-            .background(Color.Gray)
-            .fillMaxWidth()
-            .padding(10.dp)
-            .pointerInput(Unit){
-                detectDragGesturesAfterLongPress { change, dragAmount ->
-                    Log.d("MyLog", "LongPress")
-
-                }
-            }
-            .clickable {
-                Log.d("MyLog", "Clicked")
-            },
-        shape = RoundedCornerShape(15.dp),
-        elevation = 5.dp
-        ){
-            Box(modifier = Modifier.background(Color.Green)){
-                Row(modifier = Modifier.background(Color.Red),
-                    verticalAlignment = Alignment.CenterVertically) {
-                    Image(painter = painterResource(
-                        id = R.drawable.ava),
-                        contentDescription = "image",
-                        contentScale = ContentScale.Crop,
-                        modifier = Modifier
-                            .padding()
-                            .size(64.dp)
-                            .clip(CircleShape)
-                        )
-                    Column(modifier = Modifier.padding(start = 16.dp)
-                        .background(Color.Yellow),
-                        ) {
-                        Text(text = name)
-                        Text(text = prof)
-                    }
-                }
-            }
+    fun CircleItem(){
+        val counter = remember{
+            mutableStateOf(0)
+        }
+        val color = remember{
+            mutableStateOf(Color.Blue)
+        }
+        Box(modifier = Modifier.size(100.dp).background(color = color.value, shape = CircleShape).clickable { when(++counter.value) {10->color.value = Color.Green }},contentAlignment = Alignment.Center){
+            Text(text = counter.value.toString(), style = TextStyle(color = Color.White), fontSize = 20.sp)
         }
     }
 }
