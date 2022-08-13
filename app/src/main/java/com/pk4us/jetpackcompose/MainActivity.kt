66432d8549e4cb1e285482218c654c5e56b331e3
PK4U_S <Stepanchuk.ei@gmail.com>
2022-08-13 19:53:15 +0300

    weatherApp - TabLayout,HorizontalPager

--- app/src/main/java/com/pk4us/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/pk4us/jetpackcompose/MainActivity.kt
@@ -3,13 +3,32 @@ package com.pk4us.jetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.pk4us.jetpackcompose.screen.MainScreen
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import com.pk4us.jetpackcompose.screen.MainCard
+import com.pk4us.jetpackcompose.screen.TabLayout
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MainScreen()
+            Image(
+                painter = painterResource(id = R.drawable.weather_bg),
+                contentDescription = "im1",
+                modifier = Modifier
+                    .fillMaxSize()
+                    .alpha(0.7f),
+                contentScale = ContentScale.FillBounds
+            )
+            Column {
+                MainCard()
+                TabLayout()
+            }
         }
     }
 }
