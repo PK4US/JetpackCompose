d17f630f2c72a252cf89496fd07981c603939f73
PK4U_S <Stepanchuk.ei@gmail.com>
2022-08-20 11:44:40 +0300

    getData() получения данных с сервера

--- app/src/main/java/com/pk4us/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/pk4us/jetpackcompose/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.pk4us.jetpackcompose
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
@@ -10,13 +12,18 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.pk4us.jetpackcompose.screen.MainCard
 import com.pk4us.jetpackcompose.screen.TabLayout
 
+const val API_KEY = "3442427ab7b944acb4892702221108"
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            getData("London",this)
             Image(
                 painter = painterResource(id = R.drawable.weather_bg),
                 contentDescription = "im1",
@@ -31,4 +38,21 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+}
+
+private fun getData(city:String,context:Context){
+    val url = "https://api.weatherapi.com/v1/forecast.json?key=" +
+            API_KEY +
+            "&q=" +
+            city +
+            "&days=1&aqi=no&alerts=no"
+    val queue = Volley.newRequestQueue(context)
+    val sRequest = StringRequest(Request.Method.GET,url,{
+        response ->
+        Log.d("MyLog","Response: $response")
+
+    },{
+        Log.d("MyLog","VolleyError: $it")
+    })
+    queue.add(sRequest)
 }
